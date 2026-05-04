d0efe1aa2e759ec1267471289a213ac0a531b016
hamid karimi <139635310+Hamidkarimi-h@users.noreply.github.com>
2026-05-05 00:18:18 +0330

    refactor: change command flag to positional argument

--- main.go
+++ main.go
@@ -3,38 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"time"
 )
 type Agency struct{
 	ID uint
 	Name string
 	Address string
 	Phone string
-	MembershipDate string
+	MembershipDate time.Time
 	EmployeeCount uint32
-	Region Region
+	Region string
 }
-type Region struct{
-	ID uint
-	Name string
-}
+
 
 func main()  {
-	command := flag.String("command", "", "Command to execute: list, get, create, edit, status")
-	region := flag.String("region", "", "Filter or apply to a specific region (optional)")
-
-	flag.Parse()
-
-	if *command == ""{
+	
+	if len(os.Args) < 2{
 		fmt.Println("Error: Please provide a command.")
-		fmt.Println("Example: go run main.go -command=list -region=Tehran")
+		fmt.Println("Example: go run main.go list -region=Tehran")
 		fmt.Println("Available commands: list, get, create, edit, status")
-		
 		return
 	}
+	command := os.Args[1]
+	myFlags := flag.NewFlagSet("myFlags", flag.ExitOnError)
+	region := myFlags.String("region", "", "Filter or apply to a specific region")
 
+	myFlags.Parse(os.Args[2:])
+
+	
 	if *region != "" {
-		fmt.Printf("=> Executing command '%s' for region '%s'...\n", *command, *region)
+		fmt.Printf("=> Executing command '%s' for region '%s'...\n", command, *region)
 	} else {
-		fmt.Printf("=> Executing command '%s' for ALL regions...\n", *command)
+		fmt.Printf("=> Executing command '%s' for ALL regions...\n", command)
 	}
 }
