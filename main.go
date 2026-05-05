633b00a3dd387c7f1b7f0089d55e66ec8befc7b8
hamid karimi <139635310+Hamidkarimi-h@users.noreply.github.com>
2026-05-06 00:56:13 +0330

    feat: add command router and implement list command

--- main.go
+++ main.go
@@ -49,13 +49,16 @@ func saveAgencies(agencies []Agency) error {
 	return os.WriteFile(datafile, data, 0644)
 }
 
+func printUsage() {
+    fmt.Println("Usage: go run main.go <command> [options]")
+    fmt.Println("Commands: list, get, create, edit, status")
+    fmt.Println("Example: go run main.go list -region=Tehran")
+}
 
 func main()  {
 	
 	if len(os.Args) < 2{
-		fmt.Println("Error: Please provide a command.")
-		fmt.Println("Example: go run main.go list -region=Tehran")
-		fmt.Println("Available commands: list, get, create, edit, status")
+		printUsage()
 		return
 	}
 	command := os.Args[1]
@@ -69,5 +72,57 @@ func main()  {
 		fmt.Printf("=> Executing command '%s' for region '%s'...\n", command, *region)
 	} else {
 		fmt.Printf("=> Executing command '%s' for ALL regions...\n", command)
+	}
+	runCommand(command, *region)
+}
+
+func runCommand(command string, region string) {
+    switch command {
+    case "list":
+        ListAgencies(region)
+    case "create":
+        fmt.Println("Create command not implemented yet.")
+    case "edit":
+        fmt.Println("Edit command not implemented yet.")
+    case "get":
+        fmt.Println("Get command not implemented yet.")
+    case "status":
+        fmt.Println("Status command not implemented yet.")
+	case "exit":
+		os.Exit(0)
+    default:
+        fmt.Printf("Unknown command: %s\n", command)
+        printUsage()
+    }
+}
+func ListAgencies(region string) {
+	agencies, err := loadAgencies()
+	if err != nil {
+		fmt.Println("Can't load agencies:", err)
+		return
+	}
+
+	if len(agencies) == 0 {
+		fmt.Println("No agencies found.")
+		return
+	}
+
+	if region != "" {
+		fmt.Println("Filtered agencies for region:", region)
+	} else {
+		fmt.Println("All agencies:")
+	}
+
+	found := false
+	for _, agency := range agencies {
+	
+		if region == "" || agency.Region == region {
+			fmt.Printf("ID: %d, Name: %s, Region: %s\n", agency.ID, agency.Name, agency.Region)
+			found = true
+		}
+	}
+
+	if !found && region != "" {
+		fmt.Printf("No agencies found for region '%s'.\n", region)
 	}
 }
